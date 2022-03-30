25a87d651c303205dd59c24f6d91a91056c423d7
Jay Logelin <jay.logelin@gmail.com>
2022-03-30 08:01:22 -0300

    updated mulfft

--- src/mulfft.rs
+++ src/mulfft.rs
@@ -1,38 +1,76 @@
 use rustfft::{algorithm::Radix4, num_complex::Complex, Fft, FftDirection, FftNum};
 
 pub struct FFTPlan {
-    pub fft: Radix4<f64>,
-    pub ifft: Radix4<f64>,
+    pub fft_processor: Radix4<f64>,
+    pub ifft_processor: Radix4<f64>,
     pub n: usize,
 }
 
 impl FFTPlan {
     pub fn new(n: usize) -> FFTPlan {
         return FFTPlan {
-            fft: Radix4::new(n, FftDirection::Forward),
-            ifft: Radix4::new(n, FftDirection::Inverse),
+            fft_processor: Radix4::new(n, FftDirection::Forward),
+            ifft_processor: Radix4::new(n, FftDirection::Inverse),
             n: n,
         };
     }
 
-    pub fn fft(&mut self, a: &Vec<u32>) -> Vec<u32> {
-        let mut v = a.iter()    
-            .map(|x| Complex::new(*x as f64, 0.0f64))
-            .collect::<Vec<_>>();            
-        let m_slice = v.as_mut_slice();
-        self.fft.process(m_slice);
-        return m_slice.iter().map(|x| x.re as u32).collect::<Vec<u32>>();
+    fn execute_reverse_torus(&mut self, a: &Vec<u32>) -> Vec<f64> {
+      let n = a.len();
+	  let n_s_two = n / 2;
+	  let two_n = n * 2;
+      let two_pm_33: f64 = 1./ 2u64.pow(32) as f64;
+
+      let mut complex_in_out = vec![Complex{ re: 0.0f64, im: 0.0f64 }; two_n];
+
+      for i in 0..n {
+        complex_in_out[i] = Complex::new(f64::try_from(a[i]).unwrap() * two_pm_33, 0.);
+      }
+      for i in 0..n {
+        complex_in_out[n+i] = Complex{ re: -complex_in_out[i].re, im: 0.0f64 };
+      }
+      self.ifft_processor.process(&mut complex_in_out);
+      
+      let mut res = vec![0.0f64; n_s_two];
+      for i in 0..n_s_two {
+        res.push(complex_in_out[2*i+1].re);
+      }
+      res
     }
 
-    pub fn ifft(&mut self, a: &Vec<u32>) -> Vec<f64> {
-        let mut v = a
-            .iter()
-            .map(|x| Complex::new(*x as f64, 0.0f64))
-            .collect::<Vec<_>>();
-            
-        let m_slice = v.as_mut_slice();
-        self.ifft.process(m_slice);
-        return m_slice.iter().map(|x| x.re).collect::<Vec<_>>();
+    fn execute_direct_torus(&mut self, a: &Vec<f64>) -> Vec<u32> {
+      let n = a.len();
+	  let n_s_two = n / 2;
+	  let two_n = n * 2;
+      let two_p32: f64 =  2u64.pow(32) as f64;
+      let one_over_n: f64 = 1. / 1024. as f64;
+      let mut complex_in_out = vec![Complex{ re: 0.0f64, im: 0.0f64 }; two_n];
+      
+      for i in 0..n {
+        complex_in_out[2*i] = Complex::new(0., 0.);
+      }
+      for i in 0..n_s_two {
+        complex_in_out[2*i+1] = Complex::new(a[i], a[i]);
+      }
+      for i in 0..n_s_two {
+        complex_in_out[two_n-1-2*i] = Complex::new(a[i], a[i]);
+      }
+      self.fft_processor.process(&mut complex_in_out);
+
+      let mut res: Vec<u32> = Vec::new();
+      res.push((complex_in_out[0].re * one_over_n * two_p32) as u32);
+      for i in 1..n {
+        res.push(-(complex_in_out[i].re * one_over_n * two_p32) as u32);
+      }
+      return res;
+    }
+
+    pub fn ifft(&mut self, input: &Vec<u32>) -> Vec<f64> {
+      return self.execute_reverse_torus(input);
+    }
+
+    pub fn fft(&mut self, a: &Vec<f64>) -> Vec<u32> {
+      return self.execute_direct_torus(a);
     }
 
     pub fn ifft_1024(&mut self, input: &[u32; 1024]) -> [f64; 1024] {
@@ -46,7 +84,7 @@ impl FFTPlan {
 
     pub fn fft_1024(&mut self, input: &[f64; 1024]) -> [u32; 1024] {
         return self
-            .fft(&input.map(|x| x as u32).to_vec())
+            .fft(&input.to_vec())
             .try_into()
             .unwrap_or_else(|v: Vec<u32>| {
                 panic!("Expected a Vec of length {} but it was {}", 1024, v.len())
@@ -63,8 +101,8 @@ impl FFTPlan {
     }
 
     pub fn poly_mul(&mut self, a: &Vec<u32>, b: &Vec<u32>) -> Vec<u32> {
-        let a_ifft = self.ifft(a);
-        let b_ifft = self.ifft(b);
+        let a_ifft = self.execute_reverse_torus(a);
+        let b_ifft = self.execute_reverse_torus(b);
         let mut mul = vec![0.0f64; self.n];
 
         let ns = self.n / 2;
@@ -74,8 +112,9 @@ impl FFTPlan {
             mul[i] = a_ifft[i] * b_ifft[i] - aimbim;
             mul[i + ns] = a_ifft[i + ns] * b_ifft[i] + arebim;
         }
-
-        return self.fft(&mul.iter().map(|x| *x as u32).collect::<Vec<u32>>());
+        return self.fft(&mul);
+        //return self.execute_direct_torus(&mul);
+        // return self.fft(&mul.iter().map(|x| *x as u32).collect::<Vec<u32>>());
     }
 }
 
