617bcebb9e8ab571ad003ffdc28629aec6bb648a
10d9e <10d9e@users.noreply.github.com>
2025-11-03 04:48:57 +0000

    deploy: 3adf1cafc4367305b0d923979743a9892de5babc

--- trait.impl/core/ops/arith/trait.Add.js
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rs_tfhe",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"rs_tfhe/tlwe/struct.TLWELv0.html\" title=\"struct rs_tfhe::tlwe::TLWELv0\">TLWELv0</a>"]]]]);
+    var implementors = Object.fromEntries([["rs_tfhe",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for &amp;<a class=\"struct\" href=\"rs_tfhe/tlwe/struct.TLWELv0.html\" title=\"struct rs_tfhe::tlwe::TLWELv0\">TLWELv0</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
